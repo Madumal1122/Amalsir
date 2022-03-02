61d01d5e15d5a6172d647cbfe92f4761ac8938c3
Madumal1122 <99229603+Madumal1122@users.noreply.github.com>
2022-03-02 09:28:26 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *janiya*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *janiya*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *janiya*:  ```" + msg + "```"
 }
 
 
